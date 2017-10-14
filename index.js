fa2234bf6204752d57c60f7009f3f82a1ed9eccd
John Lin <bigjohnlin@gmail.com>
2017-10-14 17:28:00 -0400

    search bot created

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const bingURL = 'https://api.cognitive.microsoft.com/bing/v5.0';
+const bingKey = '1a4ff9de6b4c45bb92a8dd3d06439546';
+
+var bing = require('node-bing-api')({accKey: bingKey});
 var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
@@ -13,9 +17,23 @@ app.get('/',function(req,res) {
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
+        if (msg.indexOf("/search") > -1) {
+            bingSearch(msg.substring(8, msg.length), function(search) {
+                io.emit('bing-search', search);
+            });
+        }
     });
-    console.log('a user connected');
 });
+
+function bingSearch(msg, search) {
+    bing.web(msg, {
+        responseFilter: 'Webpages'
+    }, function(error, res, body) {
+        var linkBody = body.webPages.webSearchUrl;
+        var url = linkBody.split(',');
+        search(url[0]);
+    });
+}
 
 http.listen(port, function(){
     console.log('listening on *:' + port);
