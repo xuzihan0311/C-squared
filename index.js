134ffbe4495e0ac40619f8fb463da40ed12c5d20
John Lin <bigjohnlin@gmail.com>
2017-10-14 11:43:29 -0400

    Socket.io working on local
    Need to test on azure still

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-var express = require("express");
-var app = express();
+var express = require('express');
+var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
-//Store all JS and CSS in Scripts folder.
 
 app.get('/',function(req,res) {
     res.sendFile(__dirname + '/public/views/chat-page.html');
-    //It will find and locate index.html from View or Scripts
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+    console.log('a user connected');
 });
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+    console.log('listening on *:' + port);
 });
-
-console.log("Running at Port 3000");
