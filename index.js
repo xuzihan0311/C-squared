5ef41c7931554d02f055191c918a482418d49e5e
John Lin <bigjohnlin@gmail.com>
2017-10-14 00:52:01 -0400

    html chat page sync

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-var http = require('http');
+// var http = require('http');
+//
+// var server = http.createServer(function(request, response) {
+//
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello Xing Xing and Alex!");
+//
+// });
+//
+// var port = process.env.PORT || 1337;
+// server.listen(port);
+//
+// console.log("Server running at http://localhost:%d", port);
 
-var server = http.createServer(function(request, response) {
+var http = require('http'),
+    fs = require('fs');
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Xing Xing and Alex!");
 
+fs.readFile('./Views/chat-page.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(8000);
 });
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
