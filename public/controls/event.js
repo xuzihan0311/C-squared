876ba76a7824338abdc181348f6460a034869841
Zihan Xu <Alex@lawn-128-61-58-39.lawn.gatech.edu>
2017-10-14 19:33:40 -0400

    Diabled clicking outside login/register box

--- public/controls/event.js
+++ public/controls/event.js
@@ -2,32 +2,39 @@ $( document ).ready(function() {
   
   
  
-  $("#chat-button").mouseover(function() {
+  $("#chat-btn").mouseover(function() {
     $("#chat-icon").attr("src", "../resources/chat-icon-blue.png");
   })
     
   .mouseout(function() {
-    if (!$("#chat-button").hasClass("menu-button-open")) {
+    if (!$("#chat-btn").hasClass("menu-button-open")) {
       $("#chat-icon").attr("src", "../resources/chat-icon-grey.png");
     }
   });
  
-  $("#chat-button").click(function() {
+  $("#chat-btn").click(function() {
     
   });
     
-  $("#calendar-button").mouseover(function() {
+  $("#calendar-btn").mouseover(function() {
     $("#calendar-icon").attr("src", "../resources/calendar-icon-blue.png");
   })
     
   .mouseout(function() {
-    if (!$("#calendar-button").hasClass("menu-button-open")) {
+    if (!$("#calendar-btn").hasClass("menu-button-open")) {
       $("#calendar-icon").attr("src", "../resources/calendar-icon-grey.png");
     }
   });
  
-  $("#calendar-button").click(function() {
+  $("#calendar-btn").click(function() {
     
   });
- 
+  
+  $("#profile-btn").mouseover(function() {
+    $("#profile-icon").attr("src", "../resources/profile-icon-blue.png");
+  })
+  
+  .mouseout(function() {
+    $("#profile-icon").attr("src", "../resources/profile-icon-grey.png");
+  });
 });
