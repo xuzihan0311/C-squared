59ad287797091f3cd2b71caf6adf4144a3ead981
Zihan Xu <Alex@lawn-128-61-58-39.lawn.gatech.edu>
2017-10-15 00:37:10 -0400

    Implemented js

--- public/controls/event.js
+++ public/controls/event.js
@@ -1,7 +1,5 @@
 $( document ).ready(function() {
   
-  
- 
   $("#chat-btn").mouseover(function() {
     $("#chat-icon").attr("src", "../resources/chat-icon-blue.png");
   })
@@ -13,7 +11,16 @@ $( document ).ready(function() {
   });
  
   $("#chat-btn").click(function() {
-    
+    if (!$("#chat-btn").hasClass("menu-button-open")) {
+      $("#calendar-btn").removeClass("menu-button-open");
+      $("#calendar-btn").attr("style", "background-color: transparent;");
+      $("#calendar-icon").attr("src", "../resources/calendar-icon-grey.png");
+      $("#chat-btn").addClass("menu-button-open");
+      $("#chat-btn").attr("style", "background-color: #ffffff;");
+      $("#chat-icon").attr("src", "../resources/chat-icon-blue.png");
+      $(".calendar-content").attr("style", "display: none;");
+      $(".chat-content").attr("style", "display: inline;");
+    }
   });
     
   $("#calendar-btn").mouseover(function() {
@@ -26,8 +33,31 @@ $( document ).ready(function() {
     }
   });
  
+  $(".calendar-holder").fullCalendar({
+    
+  });
+ 
+  var newEvent = 
+  {
+    title:"Test", 
+    start:new Date("October 15, 2017")
+  };
+  
+  $(".calendar-holder").fullCalendar('renderEvent', newEvent, true);
+ 
   $("#calendar-btn").click(function() {
-
+    if (!$("#calendar-btn").hasClass("menu-button-open")) {
+      $("#chat-btn").removeClass("menu-button-open");
+      $("#chat-btn").attr("style", "background-color: transparent;");
+      $("#chat-icon").attr("src", "../resources/chat-icon-grey.png");
+      $("#calendar-btn").addClass("menu-button-open");
+      $("#calendar-btn").attr("style", "background-color: #ffffff;");
+      $("#calendar-icon").attr("src", "../resources/calendar-icon-blue.png");
+      $(".chat-content").attr("style", "display: none;");
+      $(".calendar-content").attr("style", "display: inline;");
+      $(".fc-next-button").trigger("click");
+      $(".fc-prev-button").trigger("click");
+    }
   });
   
   $("#profile-btn").mouseover(function() {
@@ -37,4 +67,5 @@ $( document ).ready(function() {
   .mouseout(function() {
     $("#profile-icon").attr("src", "../resources/profile-icon-grey.png");
   });
+  
 });
